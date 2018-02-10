7394efcc088a118a2c166ad236f002a8011cc324
Zach McCormick <zmccormick6@gmail.com>
2018-02-09 19:08:42 -0600

    UI Changes, Added Player Assets

---
+++ Assets/Code/Spin.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    void Update() {
+        transform.Rotate(Vector3.up * Time.deltaTime * 4f, Space.World);
+    }
+}
