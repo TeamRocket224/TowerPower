e321fd600c54b4d77fea3bae262904708f4237e9
Zachary McCormick <zmccormick6@gmail.com>
2018-04-18 23:06:29 -0500

    Set Up StartPose

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -95,7 +95,8 @@ public class GameController : MonoBehaviour
     public void ChangeToGame()
     {
         if (State == GameState.Pick) {
-            //Player.gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("StartGame");
+            Player.gameObject.transform.GetChild(0).GetComponent<Animator>().SetTrigger("StartGame");
+            Player.gameObject.transform.GetChild(1).GetComponent<Animator>().SetTrigger("StartGame");
         }
 
         PickUI.SetActive(false);
